6b941d9aaa8e8648312c3ac2a4a3f05ce8fc546e
Geordous-Huxwell <jconl266@mtroyal.ca>
2022-11-03 13:45:07 -0600

    outputArtistOptions

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const songss = JSON.parse(songs);
+const songs = JSON.parse(songsJSON);
+const artists = JSON.parse(artistsJSON);
 
-console.log(songss);
-for(let song of songss){
+console.log(songs);
+const artistArray = [];
+artists.forEach((artist) => {
+    artistArray.push(artist.name);
+})
+
+for (let song of songs) {
     outputTableRow(song);
 }
 
-function outputTableRow(song){
-   
-    document.write(`<tr><td>${song.title}</td><td>${song.artist.name}</td><td>${song.year}</td><td>${song.genre.name}</td>
-    <td> <progress max="100" value="${song.details.popularity}"></progress></td></tr>`);
+artistArray.forEach(artistName => {
+    outputArtistOptions(artistName);
+});
+
+
+
+function outputTableRow(song) {
+    document.getElementById("song-table-body").innerHTML += `<tr><td>${song.title}</td><td>${song.artist.name}</td><td>${song.year}</td><td>${song.genre.name}</td>
+    <td> <progress max="100" value="${song.details.popularity}"></progress></td></tr>`;
 }
 
+function outputArtistOptions(artistName) {
+    document.getElementById("song-select").innerHTML += `<option value="${artistName}">${artistName}</option>`;
+}
