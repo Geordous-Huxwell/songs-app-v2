2a44657e5ba4d456c5c9dd9a079e0dbd37aa2bc3
Geordous-Huxwell <jconl266@mtroyal.ca>
2022-11-03 15:51:09 -0600

    artist filter and clear button working

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const songs = JSON.parse(songsJSON);
 const artists = JSON.parse(artistsJSON);
 
-console.log(songs);
+console.log("songs object", songs);
+console.log("sessionStorage",sessionStorage);
+
 const artistArray = [];
 artists.forEach((artist) => {
     artistArray.push(artist.name);
 })
+if (sessionStorage.getItem("artist")){
+    let artist = sessionStorage.getItem("artist");
+    let filteredSongs = songs.filter((song) =>{
+        return song.artist.name == artist;
+    })
+    buildSongTable(filteredSongs);
+} else {
+    buildSongTable(songs);
+}
 
-for (let song of songs) {
-    outputTableRow(song);
+function buildSongTable(songs){
+    for (let song of songs) {
+        outputTableRow(song);
+    }
 }
 
 artistArray.forEach(artistName => {
     outputArtistOptions(artistName);
 });
-
-
 
 function outputTableRow(song) {
     document.getElementById("song-table-body").innerHTML += `<tr><td>${song.title}</td><td>${song.artist.name}</td><td>${song.year}</td><td>${song.genre.name}</td>
@@ -23,5 +34,11 @@ function outputTableRow(song) {
 }
 
 function outputArtistOptions(artistName) {
-    document.getElementById("song-select").innerHTML += `<option value="${artistName}">${artistName}</option>`;
+    document.getElementById("artist-select").innerHTML += `<option value="${artistName}">${artistName}</option>`;
+}
+
+function filterSongs(){
+    let artist = document.getElementById("song-search-form").elements.namedItem("artist-name").value;
+    document.getElementById("song-table-body").innerHTML = '';
+    sessionStorage.setItem("artist", artist);
 }
