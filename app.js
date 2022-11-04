cfd801085ab650041621c972a51760b333523545
Geordous-Huxwell <jconl266@mtroyal.ca>
2022-11-03 20:52:45 -0600

    API call working

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
-const songs = JSON.parse(songsJSON);
+if (!localStorage.getItem("songs")) {
+    const api = "http://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php";
+    loadJSON(api, loadData);
+}
+
+const songs = JSON.parse(localStorage.getItem("songs"));
+
+//use sample-songs file as back-up if API call fails 
+if (!songs) {
+    songs = JSON.parse(songsJSON);
+}
 const artists = JSON.parse(artistsJSON);
 
 console.log("songs object", songs);
-console.log("sessionStorage",sessionStorage);
+console.log("sessionStorage", sessionStorage);
 
 const artistArray = [];
 artists.forEach((artist) => {
     artistArray.push(artist.name);
-})
-if (sessionStorage.getItem("artist")){
+});
+
+if (sessionStorage.getItem("artist")) {
     let artist = sessionStorage.getItem("artist");
-    let filteredSongs = songs.filter((song) =>{
+    let filteredSongs = songs.filter((song) => {
         return song.artist.name == artist;
-    })
+    });
     buildSongTable(filteredSongs);
 } else {
     buildSongTable(songs);
 }
 
-function buildSongTable(songs){
+//rewrite this based on lab learnings
+function loadJSON(path, success) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                success(JSON.parse(xhr.responseText));
+            }
+        }
+    };
+    xhr.open('GET', path, true);
+    xhr.send();
+}
+
+function loadData(data) {
+    console.log(data);
+    localStorage.setItem("songs", JSON.stringify(data));
+}
+
+function buildSongTable(songs) {
     for (let song of songs) {
         outputTableRow(song);
     }
@@ -37,7 +67,7 @@ function outputArtistOptions(artistName) {
     document.getElementById("artist-select").innerHTML += `<option value="${artistName}">${artistName}</option>`;
 }
 
-function filterSongs(){
+function filterSongs() {
     let artist = document.getElementById("song-search-form").elements.namedItem("artist-name").value;
     document.getElementById("song-table-body").innerHTML = '';
     sessionStorage.setItem("artist", artist);
